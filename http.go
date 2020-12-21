81221be67c8d04cb1352d9fff832bb91449250e2
Steven Hay <contact@stevenhay.dev>
2020-12-21 18:29:27 +0000

    fix: http now uses the url passed in

--- http.go
+++ http.go
@@ -20,7 +20,7 @@ func NewClient(simpleAuthSess string) Client {
 
 // Do performs the HTTP request and returns the response
 func (c Client) Do(method, url string, body io.Reader) ([]byte, error) {
-	req, err := http.NewRequest(method, KeysURL, body)
+	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return nil, err
 	}
