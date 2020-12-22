768c5001d96f5db31e9482d44bb7ddeca5993e9c
Steven Hay <contact@stevenhay.dev>
2020-12-22 03:18:09 +0000

    refactor: use goroutines and split up expired keys
    using goroutines to send requests for all of the gamekeys at the same
    time, also show expired keys seperately to unredeemed keys.

--- main.go
+++ main.go
@@ -14,13 +14,46 @@ func main() {
 	cookieInput = strings.Replace(cookieInput, "\\075", "=", 1)
 
 	client := NewClient(cookieInput)
-	gamekeys, err := GetUserGameKeys(client)
+
+	humblebundle := NewHumbleBundle(client)
+	gamekeys, err := humblebundle.GetUserGameKeys()
 	if err != nil {
 		fmt.Printf("[FAILED] %s", err.Error())
 		return
 	}
 
+	infoChan := make(chan *GameKeyInformation)
 	for _, s := range gamekeys {
-		fmt.Println(s)
+		go func(s string) {
+			info, err := humblebundle.GetInformationForGameKey(s)
+			if err != nil {
+				fmt.Printf("[FAILED] %s", err.Error())
+				return
+			}
+			infoChan <- info
+		}(s)
+	}
+
+	expiredKeys := make([]Tpkd, 0)
+	unredeemedKeys := make([]Tpkd, 0)
+	for i := 0; i < len(gamekeys); i++ {
+		v := <-infoChan
+		for _, game := range v.TpkdDict.AllTpks {
+			if game.IsExpired {
+				expiredKeys = append(expiredKeys, game)
+			} else if len(game.RedeemedKeyValue) == 0 {
+				unredeemedKeys = append(unredeemedKeys, game)
+			}
+		}
+	}
+
+	fmt.Println("--- EXPIRED GAMES ---")
+	for _, v := range expiredKeys {
+		fmt.Printf("[%s] %s\n", v.KeyType, v.HumanName)
+	}
+
+	fmt.Println("--- UNREDEEMED GAMES ---")
+	for _, v := range unredeemedKeys {
+		fmt.Printf("[%s] %s\n", v.KeyType, v.HumanName)
 	}
 }
